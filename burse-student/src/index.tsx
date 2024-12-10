19ac5cd5270fd5ecc44672ac272c87bd7fd52f7a
Иван Книженцев <iknigencev@MacBook-Pro-Ivan.local>
2024-12-10 18:57:58 +0500

    fix errors after sync

--- burse-student/src/index.tsx
+++ burse-student/src/index.tsx
@@ -1,4 +1,24 @@
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import { StoreProvider } from "./app/providers/store"
+import { ErrorBoundary } from "./app/providers/error-boundary"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./app/app"
+import { theme } from "./shared/mui"
 
-const root = createRoot(document.getElementById('root'));
-root.render(<h1>Burse frontend</h1>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <StoreProvider>
+      <ErrorBoundary>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </ErrorBoundary>
+    </StoreProvider>
+  </StrictMode>,
+)
